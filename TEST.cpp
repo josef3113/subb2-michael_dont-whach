c55a03fe5958124bf66ca446b2a97f1120214a43
josef3113 <33261454+josef3113@users.noreply.github.com>
2017-12-08 00:54:09 +0200

    whit names and TEST.cpp

---
+++ TEST.cpp
@@ -0,0 +1,23 @@
+// name :yosef sydon id:311303333  AND  name :michael katan id :304964273
+
+
+#include "Matrixh.h"   // in this #include<iostream> and using namespace std;
+
+
+void main()
+{
+	Matrixh m(2, 2);
+	m[0][0] = 2;
+	m[1][1] = 2;
+	cout << m << endl;
+	m = m;
+	const Matrixh s = -m;
+	cout << m << endl << s << endl;
+	m = s + 2 * -m * m * 2 - s;
+	cout << m << endl << s << endl;
+	cout << s[1][1] << endl;
+	
+	system("pause");
+	 
+}
+
